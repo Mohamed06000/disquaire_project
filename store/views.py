65c064293642fb665c1d8ae14323d0459ddd1b5e
Mohamed Mazy <37507960+Mohamed06000@users.noreply.github.com>
2021-07-27 22:25:52 +0100

    a view added

--- store/views.py
+++ store/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . import models
 
 
 def index(request):
     message = "Hello World!"
+    return HttpResponse(message)
+
+
+def listing(request):
+    albums = [f'<li>{album["name"]} : {artists["name"]}</li>' for album in models.ALBUMS for artists in album['artists']]
+    message = """<ul>{}</ul>""".format("\n".join(albums))
     return HttpResponse(message)
