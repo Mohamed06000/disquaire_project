0545431046c39cf816d6d546c6da9a3b1900094e
Mohamed Mazy <37507960+Mohamed06000@users.noreply.github.com>
2021-09-21 23:17:02 +0100

    db work

--- store/models.py
+++ store/models.py
@@ -2,24 +2,37 @@ from django.db import models
 
 
 class Artist(models.Model):
-  name = models.CharField(max_length=200, unique=True)
+    name = models.CharField(max_length=200, unique=True)
+
+    def __str__(self):
+        return self.name
+
 
 class Contact(models.Model):
-  email = models.EmailField(max_length=200)
-  name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200)
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
 
 class Album(models.Model):
-  create_at = models.DateTimeField(auto_now_add=True)
-  reference = models.IntegerField(null=True)
-  title = models.CharField(max_length=200, unique=True)
-  available = models.BooleanField(default=True)
-  picture = models.URLField()
-  artists = models.ManyToManyField(Artist, related_name='albums', blank=True)
+    create_at = models.DateTimeField(auto_now_add=True)
+    reference = models.IntegerField(null=True)
+    title = models.CharField(max_length=200, unique=True)
+    available = models.BooleanField(default=True)
+    picture = models.URLField()
+    artists = models.ManyToManyField(Artist, related_name='albums', blank=True)
+
+    def __str__(self):
+        return self.title
+
 
 class Booking(models.Model):
-  create_at = models.DateTimeField(auto_now_add=True)
-  contacted = models.BooleanField(default=False)
-  album = models.OneToOneField(Album, on_delete=models.CASCADE)
-  contact = models.ForeignKey(Contact, on_delete=models.CASCADE)
+    create_at = models.DateTimeField(auto_now_add=True)
+    contacted = models.BooleanField(default=False)
+    album = models.OneToOneField(Album, on_delete=models.CASCADE)
+    contact = models.ForeignKey(Contact, on_delete=models.CASCADE)
 
-
+    def __str__(self):
+        return self.contact.name
--- store/views.py
+++ store/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from . import models
+from .models import Album, Artist, Contact, Booking
 
 
 def index(request):
-    message = "Hello World!"
+    # request albums
+    albums = Album.objects.filter(available=True).order_by('-create_at')[:12]
+    # then format the request.
+    # note that we don't use album['name'] anymore but album.name
+    # because it's now an attribute.
+    formatted_albums = ["<li>{}</li>".format(album.title) for album in albums]
+    message = """<ul>{}</ul>""".format("\n".join(formatted_albums))
     return HttpResponse(message)
 
 
@@ -15,27 +21,26 @@ def listing(request):
 
 
 def detail(request, album_id):
-    id = int(album_id) # make sure we have an integer.
-    album = models.ALBUMS[id] # get the album with its id.
-    artists = " ".join([artist['name'] for artist in album['artists']]) # grab artists name and create a string out of it.
-    message = "Le nom de l'album est {}. Il a été écrit par {}".format(album['name'], artists)
+    album = Album.objects.get(pk=album_id)
+    artists = " ".join([artist.name for artist in album.artists.all()])
+    message = "Le nom de l'album est {}. Il a été écrit par {}".format(album.title, artists)
     return HttpResponse(message)
 
 
 def search(request):
     query = request.GET.get('query')
     if not query:
-        message = "Aucun artiste n'est demandé"
+        albums = Album.objects.all()
     else:
-        albums = [
-            album for album in models.ALBUMS
-            if query in " ".join(artist['name'] for artist in album['artists'])
-        ]
+        albums = Album.objects.filter(title__icontains=query)  # contains sert a rechercher une partie d'un mot et icontains pour eviter la casse
 
-        if len(albums) == 0:
+        if not albums.exists():
+            albums = Album.objects.filter(artists__name__icontains=query)
+
+        if not albums.exists():
             message = "Misère de misère, nous n'avons trouvé aucun résultat !"
         else:
-            albums = ["<li>{}</li>".format(album['name']) for album in albums]
+            albums = ["<li>{}</li>".format(album.title) for album in albums]
             message = """
                 Nous avons trouvé les albums correspondant à votre requête ! Les voici :
                 <ul>
